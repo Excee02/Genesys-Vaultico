993b72199a115a772487bf0e291d73cc29a8f7ab
Enemuo-Edo Excel Chukwuemeka <exceledo08@gmail.com>
2024-06-14 23:44:30 +0100

    final commit?

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const hamburger = document.querySelector(".hamburger");
-const dropdown = document.querySelector(".nav-menu");
+const hamburgerOpen = document.getElementById("hamburger-open");
+const hamburgerClose = document.getElementById("hamburger-close");
+const navMobile = document.querySelector(".nav-mobile")
 
-hamburger.addEventListener("onclick", () => {
-  hamburger.classList.toggle("active");
-  dropdown.classList.toggle("active");
+hamburgerOpen.addEventListener('click', () => {
+    if(navMobile.style.display === 'none'){
+        navMobile.style.display = 'block';
+        hamburgerOpen.style.display = 'none';
+        hamburgerClose.style.display = 'block';
+    } else {
+        navMobile.style.display = 'none';
+        hamburgerOpen.style.display = 'block';
+        hamburgerClose.style.display = 'none';
+    }
 });
+
+hamburgerClose.addEventListener('click', () => {
+    if(navMobile.style.display === 'block'){
+        navMobile.style.display = 'none';
+        hamburgerOpen.style.display = 'block';
+        hamburgerClose.style.display = 'none';
+    }
+});
+
+
+
+// toggle
